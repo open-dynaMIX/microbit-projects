ac77146925c955f15ca297917eca50879180d0fd
Fabio Ambauen <1833932+open-dynaMIX@users.noreply.github.com>
2022-06-05 18:25:26 +0200

    improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,38 @@
 set -e
 
 if ! [ -d "/media/MICROBIT" ]; then
-  mounted=true
-  pmount /dev/sdb MICROBIT
+    # mount micro:bit of not mounted already
+    mounted=true
+    pmount /dev/sdb MICROBIT
 fi
 
-uflash
-sync
-echo "Putting common.py"
-ufs put ./games/common.py
-sync
-echo "Putting $1"
-ufs put "$1"
+put () {
+    # instead of `ufs put`, `uflash` could also be used. But that only works for one
+    # single file.
+    # When using `ufs put`, the file to execute must be called `main.py`.
+    echo "processing files $@"
+    echo "minifying files"
+    for file in "$@"
+    do
+        pyminify --remove-literal-statements --rename-globals --output "/tmp/$(basename $file)" "$file"
+    done
+    echo "Putting minified files"
+    for file in "$@"
+    do
+        ufs put "/tmp/$(basename $file)" "$(basename $file)"
+    done
+    sync
+    echo "removing minified file"
+    rm "/tmp/$(basename $1)"
+}
 
-sync
+echo "removing all files"
+for file in $(ufs ls); do
+    echo "Removing remote file $file"
+    ufs rm "$file"; done
+put "$@"
+
 if [ "$mounted" = true ]; then
-  pumount /dev/sdb
+    # unmount micro:bit, but only if we had to mount it at the beginning.
+    pumount /dev/sdb
 fi
